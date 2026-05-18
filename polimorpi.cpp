d8b8968582f947e6a8c225438736101716b037d7
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-05-18 18:08:26 +0700

    tambah class lia

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -16,3 +16,11 @@ class joko :public seseorang {
             cout << "Pesan dari joko" << endl;
         }
 };
+
+class lia :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
